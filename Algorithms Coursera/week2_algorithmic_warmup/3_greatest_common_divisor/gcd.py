4fda16799e062b71e7b89d3adb84133e4ebef617
Saloni <saloniswagata@gmail.com>
2020-07-18 00:52:49 +0530

    Week 2 Eucledian GCD changes

--- Algorithms Coursera/week2_algorithmic_warmup/3_greatest_common_divisor/gcd.py
+++ Algorithms Coursera/week2_algorithmic_warmup/3_greatest_common_divisor/gcd.py
@@ -3,7 +3,7 @@ def gcd(a, b):
     if b==0:
         return a
     else:
-        return euclidgcd(b,a%b)
+        return gcd(b,a%b)
 
 if __name__ == "__main__":
     a, b = map(int, input().split())
