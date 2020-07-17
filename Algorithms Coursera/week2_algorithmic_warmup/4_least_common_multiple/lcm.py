25384c67692fc2934bc5993f3f4240b735e9a383
Saloni <saloniswagata@gmail.com>
2020-07-18 00:50:28 +0530

    Week 2 Optimised LCM

--- Algorithms Coursera/week2_algorithmic_warmup/4_least_common_multiple/lcm.py
+++ Algorithms Coursera/week2_algorithmic_warmup/4_least_common_multiple/lcm.py
@@ -1,15 +1,12 @@
-# Uses python3
-import sys
+def gcd(a,b): 
+    if a == 0: 
+        return b 
+    return gcd(b % a, a)
 
-def lcm_naive(a, b):
-    for l in range(1, a*b + 1):
-        if l % a == 0 and l % b == 0:
-            return l
-
-    return a*b
+def lcm(a, b):
+    return (a*b) / gcd(a,b)
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    a, b = map(int, input.split())
-    print(lcm_naive(a, b))
+    a, b = map(int, input().split())
+    print(int(lcm(a, b)))
 
