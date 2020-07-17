44fe229e78b4cd1b2630e5c2d7c27a215077dba4
Saloni <saloniswagata@gmail.com>
2020-07-18 00:34:04 +0530

    Week 2 fibonacci last digit

--- Algorithms Coursera/week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.py
+++ Algorithms Coursera/week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.py
@@ -1,19 +1,19 @@
-# Uses python3
-import sys
 
-def get_fibonacci_last_digit_naive(n):
+
+def get_fibonacci_last_digit(n):
     if n <= 1:
         return n
 
     previous = 0
     current  = 1
+    digit = 0
 
-    for _ in range(n - 1):
-        previous, current = current, previous + current
-
-    return current % 10
+    for _ in range(2,n+1):
+        digit = (previous+current)%10
+        previous=current
+        current = digit
+    return digit
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    n = int(input)
-    print(get_fibonacci_last_digit_naive(n))
+    n = int(input())
+    print(get_fibonacci_last_digit(n))
