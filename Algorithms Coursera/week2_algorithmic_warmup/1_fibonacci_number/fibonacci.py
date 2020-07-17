ae48aef742f97a1b2d54f3d6d0ff618a75548877
Saloni <saloniswagata@gmail.com>
2020-07-17 14:16:38 +0530

    Week 2 fibonacci

--- Algorithms Coursera/week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
+++ Algorithms Coursera/week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
@@ -1,9 +1,17 @@
 # Uses python3
 def calc_fib(n):
-    if (n <= 1):
-        return n
-
-    return calc_fib(n - 1) + calc_fib(n - 2)
+    a=0
+    b=1
+    f=0
+    if n==1:
+        return 0
+    elif n==2:
+        return 1
+    for i in range(3,n+1):
+        f=a+b
+        a=b
+        b=f
+    return f
 
 n = int(input())
 print(calc_fib(n))
